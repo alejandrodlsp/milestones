212f5296c72b19ba84c84625473b4d320a3aa23f
alejandrodlsp <alejandrodlsp@hotmail.es>
2025-04-17 16:35:31 +0200

    Add cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,15 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins = ENV.fetch("ALLOWED_ORIGINS") { "" }
+      .split(",")
+      .map(&:strip)
+
+    origins origins
 
     resource "*",
       headers: :any,
-      credentials: false,
-      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
+      credentials: true,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
