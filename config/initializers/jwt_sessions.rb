9d7d4b4e2c8c7038357b87a8c1d741a08e10b337
alejandrodlsp <alejandrodlsp@hotmail.es>
2025-04-25 00:49:11 +0200

    Add jwt validation

--- config/initializers/jwt_sessions.rb
+++ config/initializers/jwt_sessions.rb
@@ -5,6 +5,5 @@ puts "REDIS_URL => #{redis_url.inspect}"
 JWTSessions.encryption_key = ENV.fetch("JWT_ENCRYPTION_KEY", "milestones-secret-jwt")
 JWTSessions.token_store = :redis, {
   redis_url: redis_url,
-  redis_db_name: 0,
   token_prefix: "jwt_"
 }
