f0090b76d3e91cae9a60ceafed16b8285e8d410c
alejandrodlsp <alejandrodlsp@hotmail.es>
2025-04-25 00:46:38 +0200

    Add jwt validation

--- config/initializers/jwt_sessions.rb
+++ config/initializers/jwt_sessions.rb
@@ -1,20 +1,10 @@
 redis_url = ENV['REDIS_URL']
 
-puts "REDIS_URL => #{redis_url.inspect}" # TODO Quitar después
-if redis_url.blank?
-  raise "REDIS_URL is missing in ENV"
-end
-
-begin
-  uri = URI.parse(redis_url)
-rescue URI::InvalidURIError => e
-  raise "REDIS_URL is invalid: #{e.message}"
-end
+puts "REDIS_URL => #{redis_url.inspect}"
 
 JWTSessions.encryption_key = ENV.fetch("JWT_ENCRYPTION_KEY", "milestones-secret-jwt")
 JWTSessions.token_store = :redis, {
-  redis_host: uri.host,
-  redis_port: uri.port,
+  redis_url: redis_url,
   redis_db_name: 0,
   token_prefix: "jwt_"
 }
